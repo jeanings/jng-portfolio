7a5c3148de1f53e92d06b9620f6150a765502b44
Jeanings Ng <jeanings.ng@gmail.com>
2021-11-09 20:03:18 -0800

    Link MongoDB to frontend // Add grid components

--- application/projects/routes.py
+++ application/projects/routes.py
@@ -5,16 +5,54 @@
 from flask import Blueprint, render_template
 from flask import current_app as app
 from .assets import build_assets
+from os import environ
+from pathlib import Path
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+from .tools.colour_sets import main_colours, sub_colours 
+
+DEBUG_MODE = app.config['FLASK_DEBUG']
 
 
 # Blueprint config.
 projects_bp = Blueprint('projects_bp', __name__, 
                         static_folder='static', 
                         template_folder='templates')
-build_assets(app)
+if DEBUG_MODE == 'True':
+    build_assets(app)
+
+
+# Login variables.
+load_dotenv(Path.cwd() / '.env')
+MONGODB_ID = environ.get('MONGODB_ID')
+MONGODB_KEY = environ.get('MONGODB_KEY')
+MDB_PASS = quote_plus(MONGODB_KEY)
+
+
+# Connection to mongo server.
+client = MongoClient(f'mongodb+srv://{MONGODB_ID}:{MDB_PASS}@portfolio.8frim.mongodb.net/fudousan?retryWrites=true&w=majority')
+db = client.projectsIndex
+collection_main = db['main']
+collection_sub = db['sub']
 
 
 # Projects route.
 @projects_bp.route("/projects", methods=['GET'])
 def projects():
-    return render_template("projects.html", title="Some personal projects / jeanings.space")
+    main_projects = []
+    sub_projects = []
+
+    for main_project in collection_main.find():
+        main_projects.append(main_project)
+
+    for sub_project in collection_sub.find():
+        sub_projects.append(sub_project) 
+
+    return render_template("projects.html", 
+        title="Some personal projects / jeanings.space",
+        main_projects=main_projects,
+        sub_projects=sub_projects,
+        main_colours=main_colours['mainColours'],
+        sub_colours=sub_colours['subColours']
+    )
--- application/static/builds/projects/projects.js
+++ application/static/builds/projects/projects.js
@@ -9,7 +9,222 @@ window.onload = function() {
     container.classList.add("fade");
     container.classList.add("show");
 	// checkImagesLoaded();
+	fillGrid()
 };
+
+
+
+function fillGrid() {
+	/* -----------------------------------------------------
+		Fill in timeline grid with corresponding projects.
+	------------------------------------------------------*/
+	const styleSheet = document.styleSheets[0];
+	const yearIdString = "projects_timeline_year_";
+	const quarterElemMap = {
+		"Q1": ".projects_timeline_year_grid_jan_apr",
+		"Q2": ".projects_timeline_year_grid_apr_jul",
+		"Q3": ".projects_timeline_year_grid_jul_oct",
+		"Q4": ".projects_timeline_year_grid_oct_dec"
+	};
+	const rowMap = {
+		"1": ".row2",
+		"2": ".row3",
+		"3": ".row4",
+		"4": ".row2",
+		"5": ".row3",
+		"6": ".row4",
+		"7": ".row2",
+		"8": ".row3",
+		"9": ".row4",
+		"10": ".row2",
+		"11": ".row3"
+	};
+	const monthElemMap = {
+		"1": ".col1",
+		"2": ".col2",
+		"3": ".col3"
+	};
+	const borderStyles = {
+		"1": "dotted",
+		"2": "dashed",
+		"3": "double",
+		"4": "groove",
+		"5": "ridge",
+		"6": "dotted",
+		"7": "dashed",
+		"8": "double",
+		"9": "groove",
+		"10": "ridge",
+		"11": "dotted"
+	}
+	var subStackCounter = {};
+	let projectColours = {};
+
+
+	mainProjects.forEach((project, index) => {
+		let colour = mainColours[index];
+		
+		projectColours = {
+			...projectColours, 
+			[project.title]: colour
+		};
+
+		Object.keys(project.year).forEach(yearItem => {
+			let yearElem = document.getElementById(yearIdString.concat(yearItem));
+
+			// Create project-specific style class.
+			let newClass = ".projects_timeline_year_".concat(
+				yearItem, '_', 
+				'main_', project._id, '.hover'
+			);
+			let newStyle = '{'
+				// + 'background: ' + colour + ';'
+				+ 'border-bottom-width: 1rem !important;'
+				+ 'cursor: pointer;'
+				+ 'transition: all 300ms ease-in-out;'
+				+ '}';
+			let classSelector = newClass.replace('.', '').replace('.hover', '');
+			styleSheet.insertRule(newClass + newStyle);
+
+			Object.keys(project.year[yearItem]).forEach(quarterItem => {
+				let quarterElem = yearElem.querySelector(quarterElemMap[quarterItem]);
+
+				project.year[yearItem][quarterItem].forEach(monthItem => {
+					// Draw border "slot" for main project.
+					let monthElem = quarterElem.querySelector(monthElemMap[monthItem])
+					monthElem.style.borderBottom = colour + ' solid 0.5rem';
+					monthElem.style.transition = 'all 300ms ease-in-out';
+
+					// Add project-specific class.
+					monthElem.classList.add(classSelector);
+				});
+			});
+
+			// Add class to project elements.
+			let projectElements = document.getElementsByClassName(classSelector);
+
+			Object.values(projectElements).forEach(el => {
+				// Add event listeners for all elements of project.
+				el.addEventListener('mouseover', () => {
+					Object.values(projectElements).forEach(elem => {
+						elem.classList.add("hover");
+					});	
+				});
+
+				// Remove event listeners for all elements of project.
+				el.addEventListener('mouseout', () => {
+					Object.values(projectElements).forEach(elem => {
+						elem.classList.remove("hover");
+					});	
+				});
+
+				el.addEventListener('click', () => {
+					console.log('click: ', el);
+				});
+			});
+		});
+	});
+
+
+	let mainProjectMarker;
+	let subProjectBorderWidth = 'var(--sub-proj-border-width)';
+	let subProjectBorderWidthHover = 'var(--sub-proj-border-width-hover)';
+	let rowCounter = 1;
+
+	subProjects.forEach(project => {
+		// Track which main project the current iteration is a component of.
+		if (mainProjectMarker === undefined) {
+			// Assign initial marker.
+			mainProjectMarker = project['componentOf'];
+		} else if (mainProjectMarker === project['componentOf']) {
+			// Increment row counter if under same main project.
+			rowCounter += 1;
+		} else if (mainProjectMarker !== project['componentOf']) {
+			// Update project marker.
+			mainProjectMarker = project['componentOf'];
+
+			// Reset row counter if different main project.
+			rowCounter = 1;
+		}
+		
+		// Assign subproject colour.
+		let colour = projectColours[project.componentOf];
+
+		Object.keys(project.year).forEach(yearItem => {
+			let yearElem = document.getElementById(yearIdString.concat(yearItem));
+
+			// Create project-specific style class.
+			let newClass = ".projects_timeline_year_".concat(
+				yearItem, '_', 
+				'sub_', project._id, '.hover'
+			);
+
+			let newStyle = '{'
+				+ 'border-bottom-width: ' + subProjectBorderWidthHover + ' !important;'
+				+ 'cursor: pointer;'
+				+ 'transition: all 300ms ease-in-out;'
+				+ '}';
+
+			let classSelector = newClass.replace('.', '').replace('.hover', '');
+			styleSheet.insertRule(newClass + newStyle);
+
+			Object.keys(project.year[yearItem]).forEach(quarterItem => {
+				let quarterElem = yearElem.querySelector(quarterElemMap[quarterItem]);
+				
+				project.year[yearItem][quarterItem].forEach(monthItem => {
+					let borderStyle = borderStyles[rowCounter].concat(' ', subProjectBorderWidth);
+					
+					// Draw border "slot" for sub project.
+					let monthElem = quarterElem.querySelector(
+						rowMap[rowCounter].concat(' ', monthElemMap[monthItem])
+					);
+
+					monthElem.style.borderBottom = colour.concat(' ', borderStyle);
+					monthElem.style.opacity = "0.85";
+					monthElem.style.transition = 'all 300ms ease-in-out';
+
+					// Add project-specific class.
+					monthElem.classList.add(classSelector);
+
+
+					// Keep track of slotted "state".
+					// subStackCounter = {...subStackCounter,
+					// 	[yearItem]: {
+					// 		...subStackCounter[yearItem],
+					// 		[quarterItem]: {
+					// 			...subStackCounter[yearItem]?.[quarterItem],
+					// 			[monthItem]: 'slotted'
+					// 		},
+					// 	}
+					// };
+				});
+			});
+
+			// Add class to project elements.
+			let projectElements = document.getElementsByClassName(classSelector);
+
+			Object.values(projectElements).forEach(el => {
+				// Add event listeners for all elements of project.
+				el.addEventListener('mouseover', () => {
+					Object.values(projectElements).forEach(elem => {
+						elem.classList.add("hover");
+					});	
+				});
+
+				// Remove event listeners for all elements of project.
+				el.addEventListener('mouseout', () => {
+					Object.values(projectElements).forEach(elem => {
+						elem.classList.remove("hover");
+					});	
+				});
+
+				el.addEventListener('click', () => {
+					console.log('click: ', el);
+				});
+			});
+		});
+	});
+}
 
 
 
