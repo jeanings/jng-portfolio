5f5060d4fdeb7ba4280c948a03f898937441d424
Jeanings Ng <jeanings.ng@gmail.com>
2022-07-20 19:02:29 -0700

    Create base tree structure for filter drawer

--- application/projects/photo_diary/React/app/src/components/FilterDrawer/FilterDrawer.tsx
+++ application/projects/photo_diary/React/app/src/components/FilterDrawer/FilterDrawer.tsx
@@ -10,6 +10,17 @@ const FilterDrawer: React.FC = () => {
         A main component - container for the filters on the left.
     ------------------------------------------------------------- */
 
+    const cameras: Array<string> = [
+        'Konica IIIM', 'Nikon FE', 'Pentax MX'
+    ];
+
+    const focalLengths: Array<string> = [
+        'Wide', 'Standard', 'Long'
+    ];
+
+    const formats: Array<string> = [
+        '35mm film', '120 film', 'APS-C digital'
+    ];
 
     /* ==================================================================== 
         
@@ -39,7 +50,24 @@ const FilterDrawer: React.FC = () => {
 
     ==================================================================== */
 
+    const onFilterClick = (event: any) => {
+    
+        // console.log(event.target.ariaLabel);
 
+        switch(event.target.ariaLabel) {
+            case 'camera_item':
+                console.log('camera item click');
+                break;
+
+            case 'focalLength_item':
+                console.log('focalLength item click');
+                break;
+
+            case 'format_item':
+                console.log('format item click');
+                break;
+        }
+    };
 
     /* -----------------------------------------------------
                         CSS classes
@@ -49,6 +77,56 @@ const FilterDrawer: React.FC = () => {
 
     return (
         <div className={useMediaQueries(classBase)}>
+            
+            <div className="FilterDrawer_parameters">
+                <div className="FilterDrawer_parameters_camera">
+                    CAMERA
+                    {/* Pull data from backend, dynamic list */}
+                    {cameras.map((camera, index) => (
+                        <button className="FilterDrawer_parameters_camera_item"
+                            role="menuitemradio" aria-label="camera_item"
+                            aria-checked="false"
+                            key={'key_camera_' + index}
+                            onClick={onFilterClick}>
+                                {camera}
+                        </button>
+                    ))}
+
+                </div>
+
+                <div className="FilterDrawer_parameters_focal">
+                    FOCAL LENGTH
+                    {/* Pull data from backend, dynamic list */}
+                    {focalLengths.map((focalLength, index) => (
+                        <button className="FilterDrawer_parameters_focalLength_item"
+                            role="menuitemradio" aria-label="focalLength_item"
+                            aria-checked="false"
+                            key={'key_focalLength_' + index}
+                            onClick={onFilterClick}>
+                                {focalLength}
+                        </button>
+                    ))}
+                </div>
+
+                <div className="FilterDrawer_parameters_format">
+                    FORMAT
+                    {/* Pull data from backend, dynamic list */}
+                    {formats.map((format, index) => (
+                        <button className="FilterDrawer_parameters_format_item"
+                            role="menuitemradio" aria-label="format_item"
+                            aria-checked="false"
+                            key={'key_format_' + index}
+                            onClick={onFilterClick}>
+                                {format}
+                        </button>
+                    ))}
+                </div>
+            </div>
+
+            <div className="FilterDrawer_tags">
+                TAGS
+            </div>
+
         </div>
     );
 }
