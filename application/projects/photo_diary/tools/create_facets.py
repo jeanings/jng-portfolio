e8756543a878df612b9e55913d35da64109e7731
Jeanings Ng <jeanings.ng@gmail.com>
2022-09-07 21:58:07 -0700

    Refactor to use two parameters, request and target_field

--- application/projects/photo_diary/tools/create_facets.py
+++ application/projects/photo_diary/tools/create_facets.py
@@ -1,52 +1,53 @@
 #---------------------------------------------------------------
-#   Script to create facet stages for MongoDB aggregate method. 
+#   Script to create pipelines for MongoDB aggregate method. 
 #---------------------------------------------------------------
 
-def create_facet(facet_name, query, target_field):
+def create_pipeline(request, target_field):
     """
-    Takes a facet name, query from front end, and document field to search in.
+    Takes request from front-end and document field to search in.
     """
 
     # Sets and subsets will be different depending on target data field.
-    # Both comparands are required to be lists.
-    if facet_name == 'getMonth':
+    if target_field == 'date.month':
         operator = '$eq'
-        comparand_a = query
+        comparand_a = request
         comparand_b = "$" + target_field
-    elif facet_name == 'getTags':
+    elif target_field == 'tags':
         operator = '$setIsSubset'
-        comparand_a = query                     # subset
+        comparand_a = request                   # subset
         comparand_b = "$" + target_field        # set
     else:
         operator = '$setIsSubset'
         comparand_a = "$" + target_field        # subset
-        comparand_b = query                     # set
+        comparand_b = request                   # set
 
-    facet = { facet_name: [
-        { '$project': {
-            '_id': 1,
-            'filename': 1,
-            'date': 1,
-            'model': 1,
-            'focal_length_35mm': 1,
-            'format': 1,
-            'iso': 1,
-            'aperture': 1,
-            'shutter_speed': 1,
-            'gps': 1,
-            'tags': 1,
-            'url': 1,
-            'isSubset': {
-                operator: [
-                    comparand_a,
-                    comparand_b 
-                ]
-            },
-            '$match': {
-                'isSubset': True
+    pipeline = [
+        { 
+            '$project': {
+                '_id': 1,
+                'filename': 1,
+                'date': 1,
+                'model': 1,
+                'focal_length_35mm': 1,
+                'format': 1,
+                'iso': 1,
+                'aperture': 1,
+                'shutter_speed': 1,
+                'gps': 1,
+                'tags': 1,
+                'url': 1,
+                'isSubset': {
+                    operator: [
+                        comparand_a,
+                        comparand_b 
+                    ]
+                },
+                '$match': {
+                    'isSubset': True
+                }
             }
-        }}
-    ]}
+        }
+    ]
 
 
-    return facet
+    return pipeline
