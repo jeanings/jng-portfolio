d3a06e11e1ebe3ff2c884c252c59232f72aacfe7
Jeanings Ng <jeanings.ng@gmail.com>
2022-10-07 00:55:54 -0700

    Refactor template

--- application/projects/photo_diary/React/app/src/features/FilterDrawer/FilterDrawer.tsx
+++ application/projects/photo_diary/React/app/src/features/FilterDrawer/FilterDrawer.tsx
@@ -1,66 +1,27 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import { useAppSelector, useMediaQueries } from '../../common/hooks';
 import './FilterDrawer.css';
 
 
 
-const FilterDrawer: React.FC = () => {
+const FilterDrawer: React.FunctionComponent = () => {
     /* ------------------------------------------------------------
         A main component - container for the filters on the left.
     ------------------------------------------------------------- */
+    const filterablesState = useAppSelector(state => state.timeline.filterSelectables); 
 
-    const formats: Array<string> = [
-        '35mm film', '120 film', 'APS-C digital'
-    ];
-
-    const cameras: Array<string> = [
-        'Konica IIIM', 'Nikon FE', 'Pentax MX'
-    ];
-
-    const focalLengths: Array<string> = [
-        'Wide', 'Standard', 'Long'
-    ];
-
-    const apertures : Array<string> = [
-        '1', '1.2', '1.4', '1.7', '1.8',
-        '2', '2.8', '4', '5.6', '8', '11',
-        '16', '22', '32'
-    ];
+    let formats: Array<string> = [];
+    let cameras: Array<string> = [];
+    let films: Array<string> = [];
+    let lenses: Array<string> = [];
+    let tags: Array<string> = [];
+    let focalLengths: Array<string> = [];
     
-
-    /* ==================================================================== 
-        
-    To-do / pseudo code
-
-        1) for all items in photosList
-                highlight all options available from set of photos
-                --> dim/grey-out options that unavailable for filtering 
-        
-        2) on click of filter buttons
-                hide/show all photos containing the parameter
-                --> save filtered items in separate state to be reinstated later (?)
     
-        3) filters based on metadata parameters
-            --> see DateBar pseudo
+    const onFilterClick = (event: React.SyntheticEvent) => {
+        const filterElem = event.target as HTMLButtonElement;
 
-
-    State interaction
-        
-        1) subscribe to list of photos according to DateBar selection --> photosList: state
-
-        2) according to metadata of photos, on/off states for filter buttons
-            --> filterList: state
-                --> focalLength: state on/off etc
-
-        3) saved "removed" photos into list state for retrieval on removing filters
-
-    ==================================================================== */
-
-    const onFilterClick = (event: any) => {
-    
-        // console.log(event.target.ariaLabel);
-
-        switch(event.target.ariaLabel) {
+        switch(filterElem.ariaLabel) {
             case 'FilterDrawer-format-item':
                 console.log('format item click');
                 break;
@@ -83,121 +44,97 @@ const FilterDrawer: React.FC = () => {
                         CSS classes
     ------------------------------------------------------*/
     const classBase: string = "FilterDrawer";
-
+    const classNames: ClassNameTypes = {
+        'parent': useMediaQueries(classBase.concat("__", "parameters")),
+        'title': useMediaQueries(classBase.concat("__", "header")),
+        'options': useMediaQueries(classBase.concat("__", "options"))
+    };
+  
+    
 
     return (
         <section className={useMediaQueries(classBase)}>
-            
-            <div className={useMediaQueries(classBase.concat("__", "parameters-wrapper"))}
-                role="group" aria-label="">
+            <div className={useMediaQueries(classBase.concat("__", "parameters-container"))}
+                role="group" aria-label="FilterDrawer-container">
 
-                <div className={useMediaQueries(classBase.concat("__", "parameters"))}
-                    role="group" aria-label="FilterDrawer-format">
-                    <h1 className={useMediaQueries(classBase.concat("__", "header"))}>
-                        FORMAT
-                    </h1>
-
-                    {/* REFACTOR: Pull data from backend, dynamic list */}
-                    <div className={useMediaQueries(classBase.concat("__", "options"))}
-                        role="group" aria-label="FilterDrawer-format-options">
-
-                        {formats.map((format, index) => (
-                            <button className={"FilterDrawer" + "__format-item"}
-                                role="checkbox" aria-label="FilterDrawer-format-item"
-                                aria-checked="false"
-                                key={'key_format_' + index}
-                                onClick={onFilterClick}>
-                                    {format}
-                            </button>
-                        ))}
-
-                    </div>
-                </div>
-
-                <div className={useMediaQueries(classBase.concat("__", "parameters"))}
-                    role="group" aria-label="FilterDrawer-camera-block">
-                    <h1 className={useMediaQueries(classBase.concat("__", "header"))}>
-                        CAMERA
-                    </h1>
-
-                    {/* REFACTOR: Pull data from backend, dynamic list */}
-                    <div className={useMediaQueries(classBase.concat("__", "options"))}
-                        role="group" aria-label="FilterDrawer-camera-options">
-
-                        {cameras.map((camera, index) => (
-                            <button className={"FilterDrawer" + "__camera-item"}
-                                role="checkbox" aria-label="FilterDrawer-camera-item"
-                                aria-checked="false"
-                                key={'key_camera_' + index}
-                                onClick={onFilterClick}>
-                                    {camera}
-                            </button>
-                        ))}
-
-                    </div>
-                </div>
-
-                <div className={useMediaQueries(classBase.concat("__", "parameters"))}
-                    role="group" aria-label="FilterDrawer-focalLength">
-                    <h1 className={useMediaQueries(classBase.concat("__", "header"))}>
-                        FOCAL LENGTH
-                    </h1>
-
-                    {/* REFACTOR: Pull data from backend, dynamic list */}
-                    <div className={useMediaQueries(classBase.concat("__", "options"))}
-                        role="group" aria-label="FilterDrawer-focalLength-options">
-
-                        {focalLengths.map((focalLength, index) => (
-                            <button className={"FilterDrawer" + "__focalLength-item"}
-                                role="checkbox" aria-label="FilterDrawer-focalLength-item"
-                                aria-checked="false"
-                                key={'key_focalLength_' + index}
-                                onClick={onFilterClick}>
-                                    {focalLength}
-                            </button>
-                        ))}
-
-                    </div>
-                </div>
-
-                <div className={useMediaQueries(classBase.concat("__", "parameters"))}
-                    role="group" aria-label="FilterDrawer-aperture">
-                    <h1 className={useMediaQueries(classBase.concat("__", "header"))}>
-                        APERTURE
-                    </h1>
-
-                    {/* REFACTOR: Pull data from backend, dynamic list */}
-                    <div className={useMediaQueries(classBase.concat("__", "options"))}
-                        role="group" aria-label="FilterDrawer-aperture-options">
-
-                        {apertures.map((aperture, index) => (
-                            <button className={"FilterDrawer" + "__aperture-item"}
-                                role="checkbox" aria-label="FilterDrawer-aperture-item"
-                                aria-checked="false"
-                                key={'key_aperture_' + index}
-                                onClick={onFilterClick}>
-                                    {aperture}
-                            </button>
-                        ))}
-
-                    </div>
-                </div>
-
-                <div className={useMediaQueries(classBase.concat("__", "parameters"))}>
-                    <h1 className={useMediaQueries(classBase.concat("__", "header"))}>
-                        TAGS
-                    </h1>
-
-                    <div className={useMediaQueries(classBase.concat("__", "options"))}
-                        role="group" aria-label="FilterDrawer-tags-options">
-                        
-                    </div>
-                </div>
+                {createCategory(classNames, "format", formats, onFilterClick)}
+                {createCategory(classNames, "camera", cameras, onFilterClick)}
+                {createCategory(classNames, "film", films, onFilterClick)}
+                {createCategory(classNames, "lens", lenses, onFilterClick)}
+                {createCategory(classNames, "tags", tags, onFilterClick)}
+                {createCategory(classNames, "focalLength", focalLengths, onFilterClick)}
 
             </div>
         </section>
     );
 }
+
+
+/* =====================================================================
+    Helper functions.
+===================================================================== */
+
+/* --------------------------------------------
+    Constructor for filter drawer categories.
+-------------------------------------------- */
+function createCategory(classNames: ClassNameTypes, categoryName: string, selectables: Array<string>, 
+    onFilterClick: MouseEventHandler) {
+
+    let categoryElem;
+    if (categoryName !== "tags") {
+        categoryElem = (
+            <div className={classNames['parent']}
+                role="group" aria-label={"FilterDrawer".concat("-", categoryName)}>
+    
+                <h1 className={classNames['title']}>
+                    {categoryName !== "focalLength"
+                        ? categoryName.toUpperCase()
+                        : "FOCAL LENGTH"}
+                </h1>
+    
+                <div className={classNames['options']}
+                    role="group" aria-label={"FilterDrawer".concat("-", categoryName, "-options")}>
+    
+                    {selectables.map((selectable, index) => (
+                        <button className={"FilterDrawer".concat("__", categoryName, "-item")}
+                            role="checkbox" aria-label={"FilterDrawer".concat("-", categoryName, "-item")}
+                            aria-checked="false"
+                            key={"key".concat("_", categoryName, "_", index.toString())}
+                            onClick={onFilterClick}>
+                                {selectable}
+                        </button>
+                    ))}
+                </div>
+            </div>
+        );
+    } 
+    else {
+        categoryElem = (
+            <div className={classNames['parent']}>
+                <h1 className={classNames['title']}>
+                    {categoryName.toUpperCase()}
+                </h1>
+
+                <div className={classNames['options']}
+                    role="group" aria-label={"FilterDrawer".concat("-", categoryName, "-options")}>
+                </div>
+            </div>
+        );
+    }
+    
+    return categoryElem;
+}
+
+
+/* =====================================================================
+    Types.
+===================================================================== */
+export type ClassNameTypes = {
+    [index: string]: string,
+    'parent': string,
+    'title': string,
+    'options': string,
+};
 
 
 export default FilterDrawer;
