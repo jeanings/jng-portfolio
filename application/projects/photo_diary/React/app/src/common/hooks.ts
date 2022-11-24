cf6fd56511095557fb5de53e36511d3ed875aa26
Jeanings Ng <jeanings.ng@gmail.com>
2022-11-23 19:40:06 -0800

    Fix Mapbox flyTo method's padding affecting fitBounds, change to offset instead

--- application/projects/photo_diary/React/app/src/common/hooks.ts
+++ application/projects/photo_diary/React/app/src/common/hooks.ts
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { 
     TypedUseSelectorHook, 
     useDispatch, 
@@ -14,9 +15,41 @@ export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 
+/* --------------------------------------------
+    Get size of window, updates with resizing.
+-------------------------------------------- */
+export function useWindowSize() {
+    let windowState: { [index: string]: number } = { 
+        width: 0, 
+        height: 0
+    };
+
+    const [ windowSize, setWindowSize ] = useState(windowState);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            });
+        }
+        
+        window.addEventListener('resize', handleResize);
+
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+        
+        // Remove event listener on cleanup
+        return () => window.removeEventListener('resize', handleResize);
+    }, [window.innerHeight]);
+
+    return windowSize;
+};
+
+
 /* ----------------------------------------
     Set media query based on breakpoints.
------------------------------------------*/
+---------------------------------------- */
 export function useMediaQueries(base: string) {
     // Get booleans for each breakpoint.
     const isMobilePortrait: boolean = useMediaQuery({
