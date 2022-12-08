3b0eddba4ab94b754ba5b50872c55632a547f6f0
Jeanings Ng <jeanings.ng@gmail.com>
2022-12-08 02:02:25 -0800

    Refactor template to auto generate elements based on db docs

--- application/projects/tools/colour_sets.py
+++ application/projects/tools/colour_sets.py
@@ -4,26 +4,34 @@ sub_alpha = '0.5' + ')';
 
 main_colours = {
     'mainColours': [
-        'rgba(0,171,60,' + main_alpha,      # green - vibrant
-        'rgba(197,198,38,' + main_alpha,    # yellow - kiwi
-        'rgba(255,155,32,' + main_alpha,    # orange - pale sunset
-        'rgba(255,59,0,' + main_alpha,      # red - bright orange-red
-        'rgba(126,114,88,' + main_alpha,    # brown - wet sand
-        'rgba(31,122,194,' + main_alpha,    # blue - sky
-        'rgba(245,7,235,' + main_alpha,     # purple - Prince
-        'rgba(144,5,119,' + main_alpha      # purple - wine
+        'rgba(0, 171, 60,' + main_alpha,
+        'rgba(197, 198, 38,' + main_alpha,
+        'rgba(255, 155, 32,' + main_alpha,
+        'rgba(255, 59, 0,' + main_alpha,
+        'rgba(157, 71, 58,' + main_alpha,
+        'rgba(126, 114, 88,' + main_alpha,
+        'rgba(66, 138, 158,' + main_alpha,
+        'rgba(31, 122, 194,' + main_alpha,
+        'rgba(22, 36, 226,' + main_alpha,
+        'rgba(93, 22, 226,' + main_alpha,
+        'rgba(171, 12, 238,' + main_alpha,
+        'rgba(144, 5, 119,' + main_alpha
     ]
 }
 
 sub_colours = {
     'subColours': [
-        'rgba(0,171,60,' + sub_alpha,      # green - vibrant
-        'rgba(197,198,38,' + sub_alpha,    # yellow - kiwi
-        'rgba(255,155,32,' + sub_alpha,    # orange - pale sunset
-        'rgba(255,59,0,' + sub_alpha,      # red - bright orange-red
-        'rgba(126,114,88,' + sub_alpha,    # brown - wet sand
-        'rgba(31,122,194,' + sub_alpha,    # blue - sky
-        'rgba(245,7,235,' + sub_alpha,     # purple - Prince
-        'rgba(144,5,119,' + sub_alpha      # purple - wine
+        'rgba(0, 171, 60,' + sub_alpha,
+        'rgba(197, 198, 38,' + sub_alpha,
+        'rgba(255, 155, 32,' + sub_alpha,
+        'rgba(255, 59, 0,' + sub_alpha,
+        'rgba(157, 71, 58,' + sub_alpha,
+        'rgba(126, 114, 88,' + sub_alpha,
+        'rgba(66, 138, 158,' + sub_alpha,
+        'rgba(31, 122, 194,' + sub_alpha,
+        'rgba(22, 36, 226,' + sub_alpha,
+        'rgba(93, 22, 226,' + sub_alpha,
+        'rgba(171, 12, 238,' + sub_alpha,
+        'rgba(144, 5, 119,' + sub_alpha
     ]
 }
