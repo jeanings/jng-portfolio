682113f6d36e472a193bc2a636e4afe6333d863d
Jeanings Ng <jeanings.ng@gmail.com>
2023-02-05 09:30:33 -0800

    Update index template, styling

--- application/static/builds/index/index.js
+++ application/static/builds/index/index.js
@@ -5,48 +5,8 @@ window.onload = function() {
 	let picFrame = document.getElementsByClassName("Index__cityscape")[0];
 	let initPicFrameWidthPercent = getComputedStyle(picFrame).width;
 
-	checkImagesLoaded();
     parallaxScroll(initPicFrameWidthPercent);
-	// aboutTextObserver();
 };
-
-
-
-function checkImagesLoaded() {
-	/* -------------------------------------------------
-		Load all images before lifting loader overlay.
-		https://stackoverflow.com/a/60949881/14181584
-	------------------------------------------------- */
-	const loader = document.getElementsByClassName("loading")[0];
-	const container = document.getElementsByClassName("container")[0];
-	const images = document.images;
-
-	Promise.all(Array.from(images).map(img => {
-		if (img.complete) 
-			return Promise.resolve(img.naturalHeight !== 0);
-
-		return new Promise(resolve => {
-			img.addEventListener("load", () => resolve(true));
-			img.addEventListener("error", () => resolve(false));
-		});
-	}))
-		.then(results => {
-			if (results.every(response => response)) {
-				loader.classList.add("fade");
-				loader.classList.add("hide");
-				container.classList.add("fade");
-				container.classList.add("show");
-			} 
-			else {
-				console.log("Index images loaded; some may be broken.");
-				loader.classList.add("fade");
-				loader.classList.add("hide");
-				container.classList.add("fade");
-				container.classList.add("show");
-			}
-		});
-}
-
 
 
 function parallaxScroll(initPicFrameWidthPercent) {
@@ -54,7 +14,7 @@ function parallaxScroll(initPicFrameWidthPercent) {
 		Parallax scroll effect.
 	-------------------------- */
 	window.addEventListener("scroll", function(event) {
-		let container = document.getElementsByClassName("container show")[0];
+		let container = document.getElementsByClassName("container")[0];
 		let scrollWidth = container.scrollWidth;
 		let scrollHeight = container.scrollHeight;
 		let orientation = (window.innerWidth > window.innerHeight) 
@@ -97,47 +57,3 @@ function parallaxScroll(initPicFrameWidthPercent) {
 			picFrame.style.width = initPicFrameWidthPercent;
 	});
 };
-
-
-
-function aboutTextObserver() {
-	/* ----------------------------------------------
-		Fade-in text gradually as it comes to view.
-	---------------------------------------------- */
-	let observer;
-	let threshold = [0, 0.20, 0.40, 0.60, 0.80, 1];
-	let observerOptions = {
-		root: null,		// threshold based on null (viewport), not element. 
-		rootMargin: '0px',
-		threshold: threshold
-	};
-
-	// Observer constructor - calls handleObservation when threshold is crossed.
-	observer = new IntersectionObserver(handleObservation, observerOptions);
-
-	// Observe targets.
-	let para1Elem = document.getElementsByClassName("index_about_text_para1")[0];
-	let para2Elem = document.getElementsByClassName("index_about_text_para2")[0];
-	let para3Elem = document.getElementsByClassName("index_about_text_para3")[0];
-	let para4Elem = document.getElementsByClassName("index_about_text_para4")[0];
-	let elemList = [para1Elem, para2Elem, para3Elem, para4Elem];
-
-	elemList.forEach(element => {
-		observer.observe(element);
-	});
-	
-
-	function handleObservation(entries, observer) {
-		/* --------------------------------------------
-			Observer callback for fading in elements.
-		-------------------------------------------- */
-		entries.forEach(entry => {
-			if (entry.intersectionRatio >= 0.80) {
-				entry.target.style.opacity = "1";
-			} 
-			else if (entry.intersectionRatio < 0.80) {
-				entry.target.style.opacity = "0";
-			}
-		});
-	}
-}
